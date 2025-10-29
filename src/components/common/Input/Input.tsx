202d182b361440a1856861737440d5da13597416
ymseoooooo <ymseoooooo@gmail.com>
2025-10-30 01:59:50 +0900

    Input Component add

---
+++ src/components/common/Input/Input.tsx
@@ -0,0 +1,53 @@
+import { InputHTMLAttributes, ReactNode, useId } from 'react';
+
+import { cn } from '@/utils/tailwind';
+import { INPUT_SIZE_CLASSES, Size } from './input.define';
+
+interface PrefixProps {
+  position: 'left' | 'right';
+  component: ReactNode;
+}
+
+interface InputProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'size' | 'width'> {
+  inputContainerClassName?: string;
+  size?: Size;
+  label?: string;
+  prefixProps?: PrefixProps;
+}
+
+export function Input({
+  inputContainerClassName,
+  size = 'md',
+  className,
+  label,
+  prefixProps,
+  readOnly,
+  ...restProps
+}: InputProps) {
+  const id = useId();
+  return (
+    <div className="flex flex-col gap-3">
+      {label && <div className="font-medium-20 h-[27px]">{label}</div>}
+      <div
+        className={cn(
+          INPUT_SIZE_CLASSES[size],
+          'flex items-center justify-center gap-[10px] p-6',
+          'rounded-xl border border-gray-700',
+          readOnly && 'border-gray-500 bg-gray-100',
+          inputContainerClassName
+        )}
+      >
+        {prefixProps?.position === 'left' && prefixProps.component}
+
+        <input
+          id={id}
+          {...restProps}
+          className={cn('flex-1 outline-none', 'font-semibold-20 text-gray-600', className)}
+          readOnly={readOnly}
+        />
+
+        {prefixProps?.position === 'right' && prefixProps.component}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/common/Input/input.define.ts
@@ -0,0 +1,5 @@
+// figma Input size에 따른 스타일 정의
+export type Size = 'md';
+export const INPUT_SIZE_CLASSES: Record<Size, string> = {
+  md: 'h-[75px]',
+};
